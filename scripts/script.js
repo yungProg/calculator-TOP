2b616428f57d08f28e8ed0a6994cce868b6d4fe9
Gideon Boakye <122945393+yungProg@users.noreply.github.com>
2024-10-20 05:12:00 +0000

    Perform basic math operations

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,100 @@
-const expressionDisplay = document.querySelector(".expression-display");
-const resultDisplay = document.querySelector(".result-display");
 const numbers = document.querySelectorAll(".number");
+const expressionDisplay = document.querySelector(".expression-display");
 const mathOperators = document.querySelectorAll(".math-operator");
+const calculate = document.querySelector(".equals");
 
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 let result = null;
 
-function displayExpression() {
-    expressionDisplay.value = firstNumber + operator + secondNumber;
+function add(a, b) {
+    a = Number(a);
+    b = Number(b);
+    let c = a + b;
+    return c;
 }
 
-function displayResult(convertedOperator) {
-    convertedOperator
+function subtract(a, b) {
+    a = Number(a);
+    b = Number(b);
+    let c = a - b;
+    return c;
+}
+
+function multiply(a, b) {
+    a = Number(a);
+    b = Number(b);
+    let c = a * b;
+    return c;
+}
+
+function divide(a, b) {
+    a = Number(a);
+    b = Number(b);
+    let c = a / b;
+    return c;
+}
+
+function operate(operator, firstNumber, secondNumber) {
     switch (operator) {
-        case "÷":
-            convertedOperator = "/";
-            break;
-    
-        case "×":
-            convertedOperator = "*";
-            break;
-        case "−":
-            convertedOperator = "-";
-            break;
         case "+":
-            convertedOperator = "+";
+            result = add(firstNumber, secondNumber);
+            break;
+        
+        case "-":
+            result = subtract(firstNumber, secondNumber);
+            break;
+
+        case "*":
+            result = multiply(firstNumber, secondNumber);
+            break;
+        
+        case "/":
+            result = divide(firstNumber, secondNumber);
             break;
     }
-    result = Function(`return ${firstNumber}${convertedOperator}${secondNumber}`)();
-    console.log(result);
-    resultDisplay.value = result;
 }
 
 numbers.forEach(number => {
     number.addEventListener("click", () => {
-        if(!firstNumber && number.textContent != "0") {
+        if (!firstNumber || firstNumber == "0") {
             firstNumber = number.textContent;
-        } else if (firstNumber && !operator) {
+            expressionDisplay.value = firstNumber;
+        } else if (!operator && firstNumber != "0") {
             firstNumber += number.textContent;
-        } else if (firstNumber && operator) {
+            expressionDisplay.value = firstNumber;
+        } 
+        else if (firstNumber && operator) {
             secondNumber += number.textContent;
-            displayResult(operator);
+            expressionDisplay.value = secondNumber;
         }
-        displayExpression();
+        console.log(number.textContent);
+        
     })
 })
 
 mathOperators.forEach(mathOperator => {
     mathOperator.addEventListener("click", () => {
-        if(firstNumber && !secondNumber) {
-            operator = mathOperator.textContent;
-        }
-        if(secondNumber) {
-            firstNumber = firstNumber + operator + secondNumber;
-            operator = mathOperator.textContent;
+        if (secondNumber) {
+            operate(operator, firstNumber, secondNumber);
+            expressionDisplay.value = result;
+            firstNumber = result;
             secondNumber = "";
         }
-        console.log(firstNumber);
-        
-        displayExpression();
+        operator = mathOperator.value;
     })
 })
+
+calculate.addEventListener("click", () => {
+    if (!secondNumber) {
+        operate(operator, result ?? firstNumber, firstNumber);
+    } else {
+        operate(operator, firstNumber, secondNumber);
+        firstNumber = result;
+    }
+    expressionDisplay.value = result;
+    secondNumber = "";
+})
+
+console.log(undefined ?? 7);
