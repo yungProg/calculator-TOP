3ce6cf0965260acef1ae01c085242adbbe9dcf52
Gideon Boakye <122945393+yungProg@users.noreply.github.com>
2024-10-24 05:48:31 +0000

    Approach has changed.
    Calculator should behave like iphone calculator on
    ios earlier than 18

--- scripts/script.js
+++ scripts/script.js
@@ -1,100 +1,118 @@
-const numbers = document.querySelectorAll(".number");
-const expressionDisplay = document.querySelector(".expression-display");
+const numberInput = document.querySelectorAll(".number");
+const decimalPoint = document.querySelector(".decimal-point");
 const mathOperators = document.querySelectorAll(".math-operator");
-const calculate = document.querySelector(".equals");
+const equalSign = document.querySelector(".equals");
 
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
-let result = null;
+let solution = "";
 
 function add(a, b) {
-    a = Number(a);
-    b = Number(b);
-    let c = a + b;
+    [a, b] = [Number(a), Number(b)];
+    c = a + b;
     return c;
 }
 
 function subtract(a, b) {
-    a = Number(a);
-    b = Number(b);
-    let c = a - b;
+    [a, b] = [Number(a), Number(b)];
+    c = a - b;
     return c;
 }
 
 function multiply(a, b) {
-    a = Number(a);
-    b = Number(b);
-    let c = a * b;
+    [a, b] = [Number(a), Number(b)];
+    c = a * b;
     return c;
 }
 
 function divide(a, b) {
-    a = Number(a);
-    b = Number(b);
-    let c = a / b;
+    [a, b] = [Number(a), Number(b)];
+    c = a / b;
     return c;
 }
 
-function operate(operator, firstNumber, secondNumber) {
-    switch (operator) {
+function takeInput(input) {
+    if (!firstNumber && !operator) {
+        if (input === ".") {
+            firstNumber = "0.";
+        } else {
+            firstNumber = input;
+        }
+    } else if (firstNumber && !operator) {
+        if (input === "." && firstNumber.includes(".")) {
+            return;
+        }
+        if (firstNumber === "0") {
+            firstNumber = input;
+        } else {
+            firstNumber += input;
+        }
+    } else if (firstNumber && operator && !secondNumber) {
+        if (input === ".") {
+            secondNumber = "0.";
+        } else {
+            secondNumber = input;
+        }
+    } else if (firstNumber && operator && secondNumber) {
+        if (input === "." && secondNumber.includes(".")) {
+            return;
+        }
+        if (secondNumber === "0") {
+            secondNumber = input;
+        } else {
+            secondNumber += input;
+        }
+    }
+    console.log(`${firstNumber} + ${operator} + ${secondNumber}`);
+}
+
+function calculate(num1, op, num2) {
+    switch (op) {
         case "+":
-            result = add(firstNumber, secondNumber);
+            solution = add(num1, num2);
             break;
-        
+    
         case "-":
-            result = subtract(firstNumber, secondNumber);
+            solution = subtract(num1, num2);
             break;
 
         case "*":
-            result = multiply(firstNumber, secondNumber);
+            solution = multiply(num1, num2);
             break;
-        
+
         case "/":
-            result = divide(firstNumber, secondNumber);
+            solution = divide(num1, num2);
             break;
     }
+    console.log(`${firstNumber} ${operator} ${secondNumber} = ${solution}`)
+    solution = solution.toString();
+    firstNumber = solution;
+    secondNumber = "";
 }
 
-numbers.forEach(number => {
-    number.addEventListener("click", () => {
-        if (!firstNumber || firstNumber == "0") {
-            firstNumber = number.textContent;
-            expressionDisplay.value = firstNumber;
-        } else if (!operator && firstNumber != "0") {
-            firstNumber += number.textContent;
-            expressionDisplay.value = firstNumber;
-        } 
-        else if (firstNumber && operator) {
-            secondNumber += number.textContent;
-            expressionDisplay.value = secondNumber;
+numberInput.forEach(item => {
+    item.addEventListener("click", () => takeInput(item.textContent))
+})
+decimalPoint.addEventListener("click", () => takeInput(decimalPoint.textContent))
+
+mathOperators.forEach(item => {
+    item.addEventListener("click", () => {
+        if (!operator) {
+            operator = item.value;
+        } else if (operator && firstNumber && !secondNumber) {
+            operator = item.value;
+        } else if (operator && secondNumber) {
+            calculate(firstNumber , operator, secondNumber);
+            operator = item.value;
         }
-        console.log(number.textContent);
-        
     })
 })
 
-mathOperators.forEach(mathOperator => {
-    mathOperator.addEventListener("click", () => {
-        if (secondNumber) {
-            operate(operator, firstNumber, secondNumber);
-            expressionDisplay.value = result;
-            firstNumber = result;
-            secondNumber = "";
-        }
-        operator = mathOperator.value;
-    })
-})
-
-calculate.addEventListener("click", () => {
+equalSign.addEventListener("click", () => {
     if (!secondNumber) {
-        operate(operator, result ?? firstNumber, firstNumber);
+        return;
     } else {
-        operate(operator, firstNumber, secondNumber);
-        firstNumber = result;
+        calculate(firstNumber, operator, secondNumber);
     }
-    expressionDisplay.value = result;
-    secondNumber = "";
 })
-
-console.log(undefined ?? 7);
